82876da35345d290b78d09f85d05595c317b2213
Matthew Bunday <zenceph@gmail.com>
2015-02-25 14:17:33 -0500

    Loop structure of board parsing defined.

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,11 +1,18 @@
 class Board
-  
+  def initialize(board_str)
+    board_str.split(',').each do |row_str|
+      row_str.split('').each do |cell_str|
+        print cell_str
+      end
+      puts 
+    end
+  end
 end
 
 class Cell
 
 end
 
-# board = Board.new(".XX.,....,....,XXXX")
+board = Board.new(".XX.,....,....,XXXX")
 # board.next
 # puts board
